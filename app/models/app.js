a8067b9c62839bf58e423edeb5a123074a9e0b8a
Adriana <adrianasucena@gmail.com>
2015-06-15 12:46:44 +0100

    Added the first version of the screenshot and app models

---
+++ app/models/app.js
@@ -0,0 +1,13 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+	name: DS.attr('string'),
+	icon: DS.attr('string'),
+	smallDescription: DS.attr('string'),
+	longDescription: DS.attr('string'),
+	screenshots: DS.hasMany('screenshot'),
+	startProjectDate: DS.attr('date'),
+	endProjectDate: DS.attr('date'),
+	storeLaunchDate: DS.attr('date'),
+	availableInAppStore: DS.attr('boolean')
+});
