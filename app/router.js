9feda33757b5526af65395a5afacb18914ffdb13
Adriana <adrianasucena@gmail.com>
2015-06-17 10:38:49 +0100

    pass the input form to the controller

--- app/router.js
+++ app/router.js
@@ -9,7 +9,7 @@ Router.map(function() {
   this.route('login');
   this.route('signup');
   this.route('protected');
-  this.route('add-app');
+  this.resource('add-app');
 });
 
 export default Router;
